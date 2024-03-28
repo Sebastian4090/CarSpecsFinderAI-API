eb8bd68075b60818dee8438a956a0a30242c6e94
Sebastian4090 <bastek4090@gmail.com>
2024-03-28 15:25:29 +0100

    connect to OpenAI and recieve JSON format responses

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,31 @@
-import express, { Express, urlencoded } from 'express';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
-import compression from 'compression';
-import cors from 'cors';
+import express, { Express, urlencoded } from "express";
+import morgan from "morgan";
+import compression from "compression";
+import cors from "cors";
+import "./config";
 
-import routes from './routes';
-import { connectDB } from './utils/connect';
-import log from './utils/logger';
+import routes from "./routes";
+import { connectDB } from "./utils/connect";
+import log from "./utils/logger";
 
-dotenv.config();
-
-const PORT: Number = Number(process.env.PORT);
+const PORT = Number(process.env.PORT);
 
 const app: Express = express();
 app.use(express.json());
-app.use(urlencoded({ extended: false }))
-app.use(morgan('combined'));
+app.use(urlencoded({ extended: false }));
+app.use(morgan("combined"));
 app.use(compression());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.json("success");
-})
+app.get("/", (req, res) => {
+  res.json("success");
+});
 
 app.listen(PORT, async () => {
-    log.info(`Server is listening on port ${PORT}`);
+  log.info(`Server is listening on port ${PORT}`);
 
-    // database connection
-    await connectDB();
+  // database connection
+  await connectDB();
 
-    routes(app);
-})
+  routes(app);
+});
