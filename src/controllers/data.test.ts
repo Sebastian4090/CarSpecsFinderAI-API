b9ef51aeaba2877f2f1b28ec2694cec85c046d1a
Sebastian4090 <bastek4090@gmail.com>
2024-06-07 11:56:37 +0200

    Building CI/CD Pipeline

--- src/controllers/data.test.ts
+++ src/controllers/data.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import express from "express";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { MongoClient, Db, ObjectId } from "mongodb";
-import handleDataGet from "./data"; // Adjust the import to your file path
+import handleDataGet from "./data";
 
 const app = express();
 app.use(express.json());
